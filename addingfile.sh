9e5a1cfccc9e5337387ac48a19edde432afa79f8
Olduser-1 <111180617+Olduser-1@users.noreply.github.com>
2022-09-27 12:39:19 +0000

    addd shell files

---
+++ addingfile.sh
@@ -0,0 +1,2 @@
+mv Session.Miku-MD.json session.data.json
+node index.js
---
+++ start.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade
+apt install git
+apt install nodejs
+yarn install 
+yarn upgrade
+node qr.js
+bash addingfile.sh
